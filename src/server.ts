634de11c70b95aec9b289189ce50fa1cda992f41
d-freitas <davi.israel01@gmail.com>
2022-05-04 21:38:46 -0300

    feat: add email sending

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,19 @@
 import express from 'express'
+import nodemailer from 'nodemailer'
 import { prisma } from './prisma'
 
 const app = express()
 
 app.use(express.json())
+
+const transport = nodemailer.createTransport({
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  auth: {
+    user: '67c689f85f4e47',
+    pass: 'f598a19aa0eeb9'
+  }
+})
 
 app.post('/feedbacks', async (req, res) => {
   const { type, comment, screenshot } = req.body
@@ -14,6 +24,18 @@ app.post('/feedbacks', async (req, res) => {
       comment,
       screenshot,
     }
+  })
+
+  transport.sendMail({
+    from: 'Equipe Feedget <oi@feedget.com>',
+    to: 'Davi Freitas <davi.israel01@gmail.com>',
+    subject: 'Novo feedback',
+    html: [
+      '<div style="font-family: sans-serif; font-size: 16px; color: #111;">',
+      `<p>Tipo do feedback: ${type}</p>`,
+      `<p>Comentário: ${comment}</p>`,
+      '<div>'
+    ].join('\n')
   })
 
   return res.status(201).json({ data: feedback })
